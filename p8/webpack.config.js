95fe4fa3690fa59a596bb851a87f5b5900cc50b7
granger kenolane <kenolane28@gmail.com>
2022-03-25 17:26:57 +0100

    add webpack

--- p8/webpack.config.js
+++ p8/webpack.config.js
@@ -55,21 +55,34 @@ Encore
         config.corejs = 3;
     })
 
-    // enables Sass/SCSS support
-    //.enableSassLoader()
+    .copyFiles({
+        from: './assets/img',
 
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+        // optional target path, relative to the output dir
+        //to: 'images/[path][name].[ext]',
 
-    // uncomment if you use React
-    //.enableReactPreset()
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
 
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+// enables Sass/SCSS support
+//.enableSassLoader()
+
+// uncomment if you use TypeScript
+//.enableTypeScriptLoader()
+
+// uncomment if you use React
+//.enableReactPreset()
+
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+//.enableIntegrityHashes(Encore.isProduction())
+
+// uncomment if you're having problems with a jQuery plugin
+//.autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
